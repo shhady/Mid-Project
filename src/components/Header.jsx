89489aa73cc4dcefd0c1c1a990d9959b8c3bbb5b
shhady <shhadyse@gmail.com>
2022-06-02 16:21:45 +0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+const Header = () => {
+  return (
+    <div className="HeaderContainer">
+      <div>Top</div>
+    </div>
+  );
+};
+export default Header;
